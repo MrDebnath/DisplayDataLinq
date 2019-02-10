4b5a08e338e4a0551f4ccf7e5b71193e0072fc49
Arindam Debnath <ari57.g@gmail.com>
2019-02-10 01:21:15 +0000

    Initial commit

---
+++ DisplayDataLinq/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Prefix
+{
+    public static IEnumerable<string> Prefixes(IEnumerable<string> words, int length)
+    {
+        IEnumerable<string> result = words.Where(x => x.Length >= length).Select(x => x.Substring(0, length)).Distinct();
+        return result;
+    }
+
+    public static void Main(string[] args)
+    {
+        foreach (var p in Prefixes(new string[] { "many", "manly", "men", "maybe", "my" }, 3))
+            Console.WriteLine(p);
+    }
+}
